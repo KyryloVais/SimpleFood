a6273f14ac4c14d5605a39a3306f2052ddd30060
KyryloVais <vayskyrylo25@gmail.com>
2023-03-06 15:00:41 +0200

    added rest-slider

--- app/js/main.js
+++ app/js/main.js
@@ -37,12 +37,49 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 
-  $(".reviews__items").slick({
-    dots: true,
-    easing: "linear",
-    prevArrow:
-      '<button type="button" class="slick-prev slick-prev--left"></button>',
-    nextArrow:
-      '<button type="button" class="slick-next slick-next--right"></button>',
+  const swiper = new Swiper(".reviews__slider", {
+    pagination: {
+      el: ".reviews__dots",
+      bulletClass: "reviews__dot",
+      bulletActiveClass: "reviews__dot--active",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".reviews__btn--next",
+      prevEl: ".reviews__btn--prev",
+    },
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const burger = document.querySelector(".burger");
+  const mobileMenu = document.querySelector(".menu-mob");
+  const bodyLock = document.querySelector("body");
+
+  burger.addEventListener("click", () => {
+    mobileMenu.classList.toggle("menu-mob--active");
+    if (mobileMenu.classList.contains("menu-mob--active")) {
+      bodyLock.classList.add("lock");
+    } else {
+      burger.classList.remove("burger--active");
+      bodyLock.classList.remove("lock");
+    }
+  });
+
+  document.addEventListener("click", function (e) {
+    if (e.target !== burger && e.target !== mobileMenu) {
+      burger.classList.remove("burger--active");
+      mobileMenu.classList.remove("menu-mob--active");
+      bodyLock.classList.remove("lock");
+    }
+  });
+
+  const swiper = new Swiper(".rest-slider__slider", {
+    pagination: {
+      el: ".rest-slider__dots",
+      bulletClass: "rest-slider__dot",
+      bulletActiveClass: "rest-slider__dot--active",
+      clickable: true,
+    },
   });
 });
