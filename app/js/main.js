7a724895861a6196997d6583fe857d2e2bfe5bf3
KyryloVais <vayskyrylo25@gmail.com>
2023-03-09 17:38:17 +0200

    added breadcrumbs, added catalog, added discounts, added filters, added menu-mob, added mob-filters, added pagination

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
+  $(".select-style").styler();
+
+  $(".price__input").ionRangeSlider({
+    type: "double",
+    prefix: "$",
+    onChange: function (data) {
+      $(".price__from").text(data.from);
+      $(".price__to").text(data.to);
+    },
+  });
+
   const header = document.querySelector(".header");
 
   window.onscroll = () => {
@@ -49,9 +60,7 @@ document.addEventListener("DOMContentLoaded", () => {
       prevEl: ".reviews__btn--prev",
     },
   });
-});
 
-document.addEventListener("DOMContentLoaded", () => {
   const burger = document.querySelector(".burger");
   const mobileMenu = document.querySelector(".menu-mob");
   const bodyLock = document.querySelector("body");
@@ -74,12 +83,48 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-  const swiper = new Swiper(".rest-slider__slider", {
-    pagination: {
-      el: ".rest-slider__dots",
-      bulletClass: "rest-slider__dot",
-      bulletActiveClass: "rest-slider__dot--active",
-      clickable: true,
-    },
-  });
+  if (window.innerWidth <= 576) {
+    const restoSlider = new Swiper(".restaurant__slider", {
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+      centeredSlides: true,
+      centeredSlidesBounds: true,
+      pagination: {
+        el: ".restaurant__dots",
+        bulletClass: "restaurant__dot",
+        bulletActiveClass: "restaurant__dot--active",
+        clickable: true,
+      },
+    });
+  }
+
+  if (window.innerWidth <= 768) {
+    const restoSlider = new Swiper(".discounts__slider", {
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+      centeredSlides: true,
+      centeredSlidesBounds: true,
+      pagination: {
+        el: ".discounts__dots",
+        bulletClass: "discounts__dot",
+        bulletActiveClass: "discounts__dot--active",
+        clickable: true,
+      },
+    });
+  }
+  if (window.innerWidth <= 768) {
+    const mobileCatalog = document.querySelector(".catalog-btn");
+    closeBtn = document.querySelector(".mob-filters__btn");
+    const mobileFilters = document.querySelector(".mob-filters");
+
+    mobileCatalog.addEventListener("click", () => {
+      bodyLock.classList.add("filters-lock");
+      mobileFilters.classList.add("open");
+    });
+
+    closeBtn.addEventListener("click", () => {
+      bodyLock.classList.remove("filters-lock");
+      mobileFilters.classList.remove("open");
+    });
+  }
 });
